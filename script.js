f93bf9b434f695c1c3ac6a758dd55c86a2cd30ae
kING-GLITCH-sudo <gksti789@gmail.com>
2025-05-16 08:52:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,59 @@
-// Verifica se o bot está digitando ou gravando áudio e atualiza o status
-setInterval(() => {
-  const isTyping = botBody.querySelector('.bubble1');
-  const sibling = isTyping?.parentElement?.parentElement?.nextSibling;
+function criarBarra(var_nome, var_avatar) {
+  // Localiza o container principal do Typebot
+  var elementoPai = document.getElementsByTagName("typebot-standard")[0].shadowRoot.querySelector('.typebot-container');
 
-  const inputContainer = botBody.querySelector('.typebot-input-container');
-  if (inputContainer){
-    inputContainer.style.width = botBody.offsetWidth + "px"
+  // Cria a barra de topo
+  var userBar = document.createElement("div");
+  userBar.className = "user-bar";
+
+  // Botão Voltar
+  var backButton = document.createElement("div");
+  backButton.className = "back";
+  backButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+</svg>`;
+
+  // Avatar
+  var avatar = document.createElement("div");
+  avatar.className = "avatar";
+  avatar.innerHTML = `<img src="${var_avatar}">`;
+
+  // Nome e status
+  var varDivNameAndStatus = document.createElement("div");
+  varDivNameAndStatus.className = "name-status-div";
+
+  var varName = document.createElement("div");
+  varName.className = "name";
+  varName.innerHTML = `<div style="display: flex;">${var_nome}<span data-testid="psa-verified"><svg viewBox="0 0 18 18" height="18" width="18" preserveAspectRatio="xMidYMid meet"><polygon fill="#27a1f9" points="..."/><polygon fill="#FFFFFF" points="..."/></svg></span></div>`;
+
+  var varStatus = document.createElement("span");
+  varStatus.className = "status";
+  varStatus.innerText = "Atendimento";
+
+  varDivNameAndStatus.appendChild(varName);
+  varDivNameAndStatus.appendChild(varStatus);
+
+  userBar.appendChild(backButton);
+  userBar.appendChild(avatar);
+  userBar.appendChild(varDivNameAndStatus);
+
+  if (elementoPai) {
+    elementoPai.prepend(userBar);
   }
 
-  if (isTyping && sibling?.src) {
-    status.innerText = 'gravando áudio...';
-  } else if (isTyping) {
-    status.innerText = 'digitando...';
-  } else {
-    status.innerText = 'online';
-  }
+  const botBody = elementoPai;
+  const status = varStatus;
 
-  // Atualiza o status das mensagens
-  const allMessages = botBody.querySelector('.typebot-chat-view').querySelectorAll('.items-start.typebot-host-bubble');
-  if (allMessages.length > mensagesLength) {
-    if (!isTyping) {
-      for (let i = mensagesLength; i < allMessages.length; i++) {
-        const date = new Date();
-        const hour = (date.getHours() < 10 ? '0' : '') + date.getHours();
-        const min = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
-        const timeNow = document.createElement('div');
-        timeNow.innerHTML = `<div class="timeNow text-xs" style="position: relative; bottom: 0; right: 0px; color: #aaaaaa;">${hour}:${min}</div>`;
-        const iconContainer = document.createElement('div');
-        iconContainer.innerHTML = `
-          <svg id="checkIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38.626 24.684" class="check-icon">
-            <g transform="translate(-708.9 -601.383)">
-              <path d="M728.035,623.468l1.382,1.482,17.929-20.334" fill="none" stroke="#36d075" stroke-linecap="round" stroke-width="3"></path>
-              <path d="M712.017,616.07l7.088,8.039,17.757-20.14" fill="none" stroke="#36d075" stroke-linecap="round" stroke-width="3"></path>
-            </g>
-          </svg>`;
-        allMessages[i].append(timeNow);
-        allMessages[i].append(iconContainer);
-      }
-      mensagesLength = allMessages.length;
-      audioNot.play();
+  setInterval(() => {
+    const isTyping = botBody.querySelector('.bubble1');
+    const sibling = isTyping?.parentElement?.parentElement?.nextSibling;
+
+    if (isTyping && sibling?.src) {
+      status.innerText = 'gravando áudio...';
+    } else if (isTyping) {
+      status.innerText = 'digitando...';
+    } else {
+      status.innerText = 'Atendimento';
     }
-  }
-}, 100);
+  }, 100);
 }
